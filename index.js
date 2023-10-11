cd8e7d58d3c7a5031b6b1b235bac6357dd229098
Jobayer Ahmed <mjobayer183@gmail.com>
2023-10-11 07:13:07 +0600

    todo app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add remove button
 
-var stranger =document.querySelector("h5");
+/*var stranger =document.querySelector("h5");
 var add = document.querySelector("#add");
 var check=0;
 
@@ -21,3 +21,34 @@ add.addEventListener ("click",function(){
     }
     
 })
+*/
+
+//to do app
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+listContainer.addEventListener("click", function(e){
+
+    if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+    }
+
+}, false);
